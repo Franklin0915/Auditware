7779561f8802f3641be2d14e683776d2bc0f6377
frank <franklinokrah465@gmail.com>
2024-01-31 14:56:24 +0000

    updated carousel

---
+++ src/Pages/Auth/Payment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Wrapper from "./Components/Wrapper";
+
+function Payment(){
+
+    return <Wrapper content={<>
+    
+    
+    
+    
+    </>}/>
+}
+export default Payment
